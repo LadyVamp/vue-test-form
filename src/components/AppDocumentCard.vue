9a6e47659fcd8091c85b43a8d068dd6f754a623d
LadyVamp <ladyvamp666@gmail.com>
2021-06-01 17:40:34 +0300

    get request using fetch

--- src/components/AppDocumentCard.vue
+++ src/components/AppDocumentCard.vue
@@ -4,10 +4,13 @@
     <div class="content">
       <section>
         <h3>Общие характеристики</h3>
-        <p>
-          <label for="num">Номер</label>
-          <input id="num" v-model="num" type="text" name="num"/>
-        </p>
+        <div v-if="data">
+          <p>{{ data.num }}</p>
+          <p>{{ data.reg_date }}</p>
+          <p>{{ data.source[0].source_name }}</p>
+          <p>{{ data.source_ed_id }}</p>
+          <p>{{ data.save_period }}</p>
+        </div>
       </section>
       <section>
         <h3>Дополнительные характеристики</h3>
@@ -20,9 +23,26 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      data: null,
+    };
+  },
+  created() {
+    // Simple GET request using fetch
+    fetch(
+      "https://gist.githubusercontent.com/LadyVamp/bed9499391bb8286d047bc966fdba4bc/raw/36fe37536fe6a41279f2e9390c82e4dae48a82de/test-data1"
+    )
+      .then((response) => response.json())
+      // .then((data) => (this.data = data));
+      .then((data) => {
+        console.log(data);
+        this.data = data;
+      });
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
