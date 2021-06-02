505b5b647d990760c7d7381c9c7582a0eb0c8da4
LadyVamp <ladyvamp666@gmail.com>
2021-06-02 11:26:39 +0300

    upd data

--- src/components/AppDocumentCard.vue
+++ src/components/AppDocumentCard.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="card">
-    <h2>Документ 1</h2>
+    <h2 v-if="data">{{ data.name }}</h2>
     <div class="content">
       <section>
         <h3>Общие характеристики</h3>
         <div v-if="data">
           <p>{{ data.num }}</p>
           <p>{{ data.reg_date }}</p>
-          <p>{{ data.source[0].source_name }}</p>
+          <p>{{ data.source_name }}</p>
           <p>{{ data.source_ed_id }}</p>
           <p>{{ data.save_period }}</p>
         </div>
@@ -32,10 +32,9 @@ export default {
   created() {
     // Simple GET request using fetch
     fetch(
-      "https://gist.githubusercontent.com/LadyVamp/bed9499391bb8286d047bc966fdba4bc/raw/36fe37536fe6a41279f2e9390c82e4dae48a82de/test-data1"
+      "https://gist.githubusercontent.com/LadyVamp/bed9499391bb8286d047bc966fdba4bc/raw/feee121c0d5ad23295e279b79c94736b1925ac74/test-data1"
     )
       .then((response) => response.json())
-      // .then((data) => (this.data = data));
       .then((data) => {
         console.log(data);
         this.data = data;
